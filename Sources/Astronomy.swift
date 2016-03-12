2e8a16dc64760a365f6a1cdd83ca0e0191f203af
Alex Hamilton <alex@bflatstudios.com>
2016-03-12 15:46:04 -0600

    Add ecliptic longitude stuff and fix misspellings of "anomaly"

--- Sources/Astronomy.swift
+++ Sources/Astronomy.swift
@@ -4,6 +4,17 @@ let earthObliquity = 23.4397 * radianConversionFactor
 
 typealias Angle = Float
 
-func solarMeanAnomoly(date date: NSDate) -> Angle { /// Returns the mean anomoly (position that Earth would be in relative to its perihelion in a circular orbit) for `date`.
+func solarMeanAnomaly(date date: NSDate) -> Angle { /// Returns the mean anomaly (position that Earth would be in relative to its perihelion in a circular orbit) for `date`.
 	return (357.5291 + ((date.julianRepresentation - NSDate.Julian2000) * 0.98560028)) * radianConversionFactor
+}
+
+func eclipticLongitude(meanAnomaly anomaly: Float) -> Angle { /// Returns the ecliptic longitude of the Earth given its mean anomaly.
+	let center = (1.9148 * sin(anomaly) + 0.02 * sin(2 * anomaly) + 0.0003 * sin(3 * anomaly)) * radianConversionFactor
+	let earthPerihelion = 102.9372 * radianConversionFactor
+
+	return anomaly + center + earthPerihelion + π
+}
+
+func eclipticLongitude(date date: NSDate) -> Angle { /// Convenience method that returns ecliptic longitude given only a date by first calculating hte mean anomaly.
+	return eclipticLongitude(meanAnomaly: solarMeanAnomaly(date: date))
 }
