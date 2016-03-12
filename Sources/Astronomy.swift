167508b1726a68fd1f81187f056bebb7f571fd77
Alex Hamilton <alex@bflatstudios.com>
2016-03-12 17:10:19 -0600

    Add altitude, azimuth, sidereal time, and refraction stuff.

--- Sources/Astronomy.swift
+++ Sources/Astronomy.swift
@@ -30,23 +30,63 @@ func declination(date date: NSDate) -> Angle { /// Convenience method that retur
 func rightAscension(eclipticLongitude longitude: Angle, eclipticLatitude latitude: Angle) -> Angle { /// Returns the right ascension (α) of the Sun given ecliptic latitude and longitude.
 	let y = sin(longitude) * cos(earthObliquity) - tan(latitude) * sin(earthObliquity)
 	let x = cos(longitude)
-	return atan(y/x)
+	return atan(y/x) // TODO: Investigate to ensure this is actually correct
 }
 
 func rightAscension(date date: NSDate) -> Angle { /// Convenience method that returns the right ascension of the Sun at a given date.
 	return rightAscension(eclipticLongitude: eclipticLongitude(date: date), eclipticLatitude: eclipticLatitude(date: date))
 }
 
+func azimuth(time time: Angle, latitude: Angle, declination: Angle) -> Angle {
+	let y = sin(time)
+	let x = (cos(time) * sin(latitude)) - (tan(declination) * cos(latitude))
+	return atan(y/x)
+}
+
+func altitude(time time: Angle, latitude: Angle, declination: Angle) -> Angle {
+	return asin((sin(latitude) * sin(declination)) + (cos(latitude) * cos(declination) * cos(time)))
+}
+
+func siderealTime(days days: Float, longitude: Float) -> Angle { /// Returns the sidereal time for the day `days` days after Julian 2000 at the given longitude.
+	return (280.1600 + (360.9856235 * days) - longitude) * radianConversionFactor
+}
+
+func siderealTime(date date: NSDate, longitude: Float) -> Angle { /// Convenience method for `siderealTime(days:longitude:)`.
+	return siderealTime(days: date.daysSince2000, longitude: longitude)
+}
+
+func refraction(altitude: Float) -> Float { /// Returns the astronomical refraction for the given altitude (in radians).
+	let h = max(altitude, 0) // Positive altitudes only
+	return 0.0002967 / tan(h + 0.00312536 / (h + 0.08901179)) // Here be dragons
+}
+
 struct SunCoordinates {
 
-	var declination: Angle
-	var rightAscension: Angle
+	let declination: Angle
+	let rightAscension: Angle
 
 	init(date d: NSDate?) {
 		let date = d ?? NSDate()
 		let longitude = eclipticLongitude(date: date)
 		declination = declination(longitude, 0)
 		rightAscension = rightAscension(longitude, 0)
+	}
+
+}
+
+Struct SunPosition {
+
+	let azimuth: Float
+	let altitude: Float
+
+	init(date d: NSDate?, latitude: Float) { // Latitude in degrees
+		let date = d ?? NSDate()
+		let latitude = radianConversionFactor * latitude
+		let longitude = radianConversionFactor * -longitude
+		let coordinates = SunCoordinates(date: date)
+		let time = siderealTime(date: date, longitude: longitude) - coordinates.rightAscension
+		azimuth = azimuth(time: time, latitude: latitude, declination: coordinates.declination)
+		altitude = altitude(time: time, latitude: latitude, declination: coordinates.declination)
 	}
 
 }
