5a247e21a11fa36364f30f24a06c22352294093c
Alex Hamilton <alex@bflatstudios.com>
2016-03-12 14:24:32 -0600

    Rename rad to more verbose radianConversionFactor.

--- Sources/Trigonometry.swift
+++ Sources/Trigonometry.swift
@@ -5,5 +5,5 @@ import Darwin
 #endif
 
 public let π = M_PI
-public let rad = π/180
+public let radianConversionFactor = π/180 // This is just as legible as an extension on number types would be, and less crowding.
 
