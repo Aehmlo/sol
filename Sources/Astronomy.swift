8799509d36766e2d3d40efa0e204f6cbe873b270
Alex Hamilton <alex@bflatstudios.com>
2016-03-13 14:24:19 -0500

    Float->Double

--- Sources/Astronomy.swift
+++ Sources/Astronomy.swift
@@ -8,7 +8,7 @@ import Darwin
 
 let earthObliquity = 23.4397 * radianConversionFactor
 
-typealias Angle = Float
+typealias Angle = Double
 
 // General solar stuff
 
@@ -20,7 +20,7 @@ func solarMeanAnomaly(date date: NSDate) -> Angle { /// Returns the mean anomaly
 
 // Ecliptic coordinates
 
-func eclipticLongitude(meanAnomaly anomaly: Float) -> Angle { /// Returns the ecliptic longitude of the Earth given its mean anomaly.
+func eclipticLongitude(meanAnomaly anomaly: Angle) -> Angle { /// Returns the ecliptic longitude of the Earth given its mean anomaly.
 	let center = (1.9148 * sin(anomaly) + 0.02 * sin(2 * anomaly) + 0.0003 * sin(3 * anomaly)) * radianConversionFactor
 	let earthPerihelion = 102.9372 * radianConversionFactor
 
@@ -67,22 +67,22 @@ func altitude(time time: Angle, latitude: Angle, declination: Angle) -> Angle {
 	return asin((sin(latitude) * sin(declination)) + (cos(latitude) * cos(declination) * cos(time)))
 }
 
-func siderealTime(days days: Float, longitude: Float) -> Angle { /// Returns the sidereal time for the day `days` days after Julian 2000 at the given longitude.
+func siderealTime(days days: Double, longitude: Double) -> Angle { /// Returns the sidereal time for the day `days` days after Julian 2000 at the given longitude.
 	return (280.1600 + (360.9856235 * days) - longitude) * radianConversionFactor
 }
 
-func siderealTime(date date: NSDate, longitude: Float) -> Angle { /// Convenience method for `siderealTime(days:longitude:)`.
+func siderealTime(date date: NSDate, longitude: Double) -> Angle { /// Convenience method for `siderealTime(days:longitude:)`.
 	return siderealTime(days: date.daysSince2000, longitude: longitude)
 }
 
-func refraction(altitude: Float) -> Float { /// Returns the astronomical refraction for the given altitude (in radians).
+func refraction(altitude: Double) -> Double { /// Returns the astronomical refraction for the given altitude (in radians).
 	let h = max(altitude, 0) // Positive altitudes only
 	return 0.0002967 / tan(h + 0.00312536 / (h + 0.08901179)) // Here be dragons
 }
 
 // More stuff
 
-func julianCycle(days days: Float, longitude: Angle) -> Int {
+func julianCycle(days days: Double, longitude: Angle) -> Int {
 	return round(days - NSDate.Julian0 - (longitude / (2 * π)))
 }
 
@@ -90,16 +90,16 @@ func julianCycle(date date: NSDate, longitude: Angle) -> Int {
 	return julianCycle(days: date.daysSince2000, longitude: longitude)
 }
 
-func approximateTransit(time time: Angle, longitude: Angle, days: Float) -> Float {
+func approximateTransit(time time: Angle, longitude: Angle, days: Double) -> Double {
 	let n = julianCycle(days: days, longitude: longitude)
 	return NSDate.Julian0 + ((time + longitude) / (2 * π)) + n
 }
 
-func approximateTransit(time time: Angle, longitude: Angle, date: NSDate) -> Float {
+func approximateTransit(time time: Angle, longitude: Angle, date: NSDate) -> Double {
 	return approximateTransit(time: time, longitude: longitude, days: date.daysSince2000)
 }
 
-func solarTransitDay(approximateTransit transit: Float, meanAnomaly anomaly: Angle, eclipticLongitude longitude: Angle) -> Float { /// Returns the Julian day of the solar transit in question
+func solarTransitDay(approximateTransit transit: Double, meanAnomaly anomaly: Angle, eclipticLongitude longitude: Angle) -> Double { /// Returns the Julian day of the solar transit in question
 	return NSDate.Julian2000 + transit + (0.0053 * sin(anomaly)) - (0.0069 * sin(2 * longitude))
 }
 
@@ -107,7 +107,7 @@ func hourAngle(altitude altitude: Angle, latitude: Angle, declination: Angle) ->
 	return acos((sin(altitude) - (sin(latitude) * sin(declination))) / (cos(latitude) * cos(declination)))
 }
 
-func sunsetDay(altitude altitude: Angle, longitude: Angle, latitude: Angle, declination: Angle, days: Float, meanAnomaly: Angle, eclipticLongitude: Angle) -> Float { /// Returns the Julian day for sunset time 
+func sunsetDay(altitude altitude: Angle, longitude: Angle, latitude: Angle, declination: Angle, days: Double, meanAnomaly: Angle, eclipticLongitude: Angle) -> Double { /// Returns the Julian day for sunset time 
 	let transit = approximateTransit(time: hourAngle(altitude: altitude, longitude: longitude, declination: declination), longitude: longitude, days: days)
 	return solarTransitDay(approximateTransit: transit, meanAnomaly: meanAnomaly, eclipticLongitude: eclipticLongitude)
 }
@@ -130,10 +130,10 @@ struct SunCoordinates {
 
 struct SunPosition {
 
-	let azimuth: Float
-	let altitude: Float
+	let azimuth: Double
+	let altitude: Double
 
-	init(date d: NSDate?, latitude lat: Float, longitude long: Float) { // Latitude, longitude in degrees
+	init(date d: NSDate?, latitude lat: Double, longitude long: Double) { // Latitude, longitude in degrees
 		let date = d ?? NSDate()
 		let lat = radianConversionFactor * lat
 		let long = radianConversionFactor * -long
